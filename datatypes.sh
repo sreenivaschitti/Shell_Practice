b52db08b79f36e02896f035b34e6e8d88c310d26
sreenivas <sreenivaschitti@gmail.com>
2026-01-29 10:45:49 +0530

    datatypes

--- datatypes.sh
+++ datatypes.sh
@@ -4,4 +4,11 @@ NAME=Sree
 PHONE=9949504909
 SKILL=("devops" "aws" "cicd")
 
-echo " $NAME with $PHONE having skills ${SKILL[0-1]}"
+echo " $NAME with $PHONE having skills ${SKILL[0]}"
+
+if [$SKILL[0]="devops"]; then
+
+
+    echo "$SkILL[0]"
+
+fi
