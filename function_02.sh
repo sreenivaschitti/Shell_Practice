bdcc0843a851695f33e4f47da6176e8a272b80de
sreenivas <sreenivaschitti@gmail.com>
2026-01-31 18:19:58 +0530

    functions

--- function_02.sh
+++ function_02.sh
@@ -18,11 +18,11 @@ VALIDATE(){
 
     if [ $1 -ne 0 ]; then
         
-        echo "$1 ....Failure" | tee -a $LOGS_FILE
+        echo "$2 ....Failure" | tee -a $LOGS_FILE
 
     else
 
-        echo "$1 ....Success" |  tee -a $LOGS_FILE
+        echo "$2 ....Success" |  tee -a $LOGS_FILE
 
     fi        
 }
@@ -48,14 +48,15 @@ for package in $@
 
 do
    
-   dnf list installed $package
+   dnf list installed $package 
 
-    if [ $? -ne 0 ]; then
-        echo "$package not installed, installing now"
+    if [ $? -eq 0 ]; then
+        echo "$package already installed, skipping"
+    else
+       
+       echo "$package not installed, installing now"
         dnf install $package -y &>>$LOGS_FILE
         VALIDATE $? "$package installation"
-    else
-       echo "$package already installed, skipping"
     fi
 
 done    
