90a680894210ae090fcf7e9ebbab05f47cd6459a
sreenivas <sreenivaschitti@gmail.com>
2026-01-31 15:34:36 +0530

    functions

---
+++ function_02.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+LOGS_FOLDER="/var/log/Shell_Practice"
+LOGS_FILE=$LOGFOLDER/$0.log
+USERID=$(id -u)
+
+if [ USERID -eq 0 ]; then
+
+    echo " run with root user" &>>LOGS_FILE
+
+fi
+
+mkdir -p $LOGFOLDER
+
+VALIDATE(){
+
+    if [ $1 -ne 0 ]; then
+        
+        echo "$1 ....Failure" &>>LOGS_FILE
+
+    else
+
+        echo "$1 ....Success" &>>LOGS_FILE
+
+    fi        
+}
+
+for package in $@
+
+ do 
+    dnf list installed $package
+    if [ $? eq 0 ]; then
+
+        echo " $package already installed "
+
+    else  
+
+        dnf install $package -y &>>$LOGS_FILE
+    
+        VALIDATE $? "$package  installation"
+ 
+ done   
