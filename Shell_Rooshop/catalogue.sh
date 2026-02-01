69811dbd4c77d77fe9044ea547c7da433ec26b62
sreenivas <sreenivaschitti@gmail.com>
2026-02-01 19:10:50 +0530

    catelog added

--- Shell_Rooshop/catalogue.sh
+++ Shell_Rooshop/catalogue.sh
@@ -6,6 +6,7 @@ LOGS_FOLDER="/var/log/Shell_Roboshop"
 LOGS_FILE=$LOGS_FOLDER/$0.log
 USERID=$(id -u)
 MONGODB_SERVER_IPADDRESS="mongodb.chittis.online"
+SCRIPT_DIR=$PWD
 
 
 if [ $USERID -ne 0 ]; then
@@ -33,14 +34,14 @@ VALIDATE(){
 dnf module disable nodejs -y &>>$LOGS_FILE
 VALIDATE $? "disable nodejs"
 
-dnf module enable nodejs:20 -y 
+dnf module enable nodejs:20 -y &>>$LOGS_FILE
 VALIDATE $? "nable nodejs:20"
 
-dnf install nodejs -y 
+dnf install nodejs -y &>>$LOGS_FILE
 VALIDATE $? "install nodejs"
 
 id roboshop 
-if [ $? -ne 0]; then
+if [ $? -ne 0 ]; then
         useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
         VALIDATE $? "user added"
 
@@ -62,32 +63,32 @@ VALIDATE $? "movig to app folder"
 rm -rf /app/*
 VALIDATE $? "removing files in app"
 
-unzip /tmp/catalogue.zip
+unzip /tmp/catalogue.zip &>>$LOGS_FILE
 VALIDATE $? "catalogue downloaded"
 
 
-npm install 
+npm install &>>$LOGS_FILE
 VALIDATE $? "npm install"
 
-cp catalogue_service /etc/systemd/system/catalogue.service
+cp $SCRIPT_DIR/catalogue_service /etc/systemd/system/catalogue.service 
 VALIDATE $? "copy catalogue service"
 
-systemctl daemon-reload
+systemctl daemon-reload &>>$LOGS_FILE
 VALIDATE $? "systemctl daemon-reload"
 
 
-systemctl enable catalogue 
+systemctl enable catalogue &>>$LOGS_FILE
 VALIDATE $? "systemctl enable catalogue"
 
-systemctl start catalogue
+systemctl start catalogue &>>$LOGS_FILE
 VALIDATE $? "start catalogue"
 
 cp  mongo.repo /etc/yum.repos.d/mongo.repo
 
-dnf install mongodb-mongosh -y
+dnf install mongodb-mongosh -y &>>$LOGS_FILE
 VALIDATE $? "mongodb-mongosh"
 
-mongosh --host $MONGODB_SERVER_IPADDRESS </app/db/master-data.js
+mongosh --host $MONGODB_SERVER_IPADDRESS </app/db/master-data.js &>>$LOGS_FILE
 
 VALIDATE $? "mongodb-mongosh"
 
