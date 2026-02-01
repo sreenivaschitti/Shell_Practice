ed07aa9455c26cceb8a4c7a10bab8e446a827217
sreenivas <sreenivaschitti@gmail.com>
2026-02-01 14:36:39 +0530

    rooshop edited

--- Shell_Rooshop/Roboshop.sh
+++ Shell_Rooshop/Roboshop.sh
@@ -3,6 +3,7 @@
 SG_ID="sg-0aa5dff7e9a9bb856"
 AMI_ID="ami-0220d79f3f480ecf5"
 HOSTED_ZONE_ID="Z014515327I2BWOYLLNUD"
+DOMAIN_NAME="chittis.online"
 
 
 
@@ -17,7 +18,7 @@ do
         --query 'Instances[0].InstanceId' \
         --output text )
 
-        if [ $instance -eq "frontend" ]; then
+        if [ $instance == "frontend" ]; then
 
              IP=$(
                aws ec2 describe-instances \
@@ -25,6 +26,8 @@ do
                --query 'Reservations[].Instances[].PublicIpAddress' \
                --output text
              )
+
+             RECORD_NAME=$DOMAIN_NAME
         else
             IP=$(
                aws ec2 describe-instances \
@@ -33,8 +36,30 @@ do
                --output text
              )
 
+             RECORD_NAME=$instance.$DOMAIN_NAME
+
         fi
 
     echo "Ip adress $IP"
 
+
+    aws route53 change-resource-record-sets \
+     --hosted-zone-id $HOSTED_ZONE_ID \
+     --change-batch '
+     {
+        "Comment": "Update A record ",
+        "Changes": [
+            {
+                  "Action": "UPSERT",
+                  "ResourceRecordSet": {
+                   "Name": '$RECORD_NAME',
+                   "Type": "A",
+                   "TTL": 1,
+                  "ResourceRecords": [{ "Value": "'$IP'" }]
+              }
+            }
+         ]
+     }
+     '
+            echo "record updated for $instance"
 done
