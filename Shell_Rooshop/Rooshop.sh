b4d6f3043713f5ab307f90a4037457592a965946
sreenivas <sreenivaschitti@gmail.com>
2026-02-01 13:47:59 +0530

    mongodb

--- Shell_Rooshop/Rooshop.sh
+++ Shell_Rooshop/Rooshop.sh
@@ -5,33 +5,6 @@ AMI_ID="ami-0220d79f3f480ecf5"
 HOSTED_ZONE_ID="Z014515327I2BWOYLLNUD"
 
 
-LOGS_FOLDER="/var/log/Rooshop_Shell"
-LOGS_FILE=$LOGS_FOLDER/$0.log
-USERID=$(id -u)
-
-
-if [ $USERID -ne 0 ]; then
-
-    echo " run with root user" | tee -a $LOGS_FILE
-    exit 1
-
-fi
-
-mkdir -p $LOGS_FOLDER
-
-VALIDATE(){
-
-    if [ $1 -ne 0 ]; then
-        
-        echo "$2 ....Failure" | tee -a $LOGS_FILE
-
-    else
-
-        echo "$2 ....Success" |  tee -a $LOGS_FILE
-
-    fi        
-}
-
 
 for instance in $@
 
