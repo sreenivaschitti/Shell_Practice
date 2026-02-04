3e632fab5463cf6abc020d9374e41fa39a7b5894
sreenivas <sreenivaschitti@gmail.com>
2026-02-04 12:28:01 +0530

    mongo

--- Shell_Rooshop/catalogue.sh
+++ Shell_Rooshop/catalogue.sh
@@ -2,11 +2,11 @@
 source ./common.sh
 app_name=catalogue
 MONGODB_SERVER_IPADDRESS=mongodb.chittis.online
-chekck_root()
+chekck_root
 
-app_setup()
-nodejs_setup()
-SYSTEM_SETUP()
+app_setup
+nodejs_setup
+SYSTEM_SETUP
 
 #Loading data into mongod
 cp  $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
@@ -28,7 +28,7 @@ echo -e "$(date +"%Y-%m-%d %H:%M:%S") | Script executed in $TOTAL_TIME seconds $
 
 fi
 
-app_restart()
+app_restart
 
 
 
