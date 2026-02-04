9f0e66ef14e8dbda22c1caea0d436f757ad1baef
sreenivas <sreenivaschitti@gmail.com>
2026-02-04 14:20:58 +0530

    mongo

--- Shell_Rooshop/user.sh
+++ Shell_Rooshop/user.sh
@@ -2,9 +2,9 @@
 source ./common.sh
 app_name=user
 MONGODB_SERVER_IPADDRESS=mongodb.chittis.online
-chekck_root()
+chekck_root
 
-app_setup()
-nodejs_setup()
-SYSTEM_SETUP()
+app_setup
+nodejs_setup
+SYSTEM_SETUP
 
