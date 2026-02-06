c4a8c6629d84940e2dda9c683c1db15c3e821d1d
sreenivas <sreenivaschitti@gmail.com>
2026-02-06 14:30:14 +0530

    practice

---
+++ Shell/backup_data.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+SOURE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:  -14}
+USERID=$(id -u)
+LOGS_DIR="/var/log/backup"
+LOGS_FILE="$LOGS_FILE/$0.log"
+
+
+if [ $USERID -ne 0 ]; then
+
+echo "please run with ROOT user" &>$LOGS_FILE
+
+fi
+
+mkdir $LOGS_DIR
+
+USAGE(){
+
+echo " source and destination files required"
+exit 1
+
+}
+
+log(){
+
+    echo " $(date '+%Y-%m-%d %H:%M:%S') | $1 " | tee -a $LOGS_FILE
+}
+
+if [ &# -lt 2]; then
+
+USAGE
+
+fi
+
+
+if [! -d $SOURE_DIR]; then
+
+echo "$SOURE_DIR is not available"
+exit 1
+
+fi
+
+if [! -d $DEST_DIR_DIR]; then
+
+echo "$SOURE_DIR is available"
+exit 1
+
+fi
+
+FILES=$( find $SOURE_DIR -type f -mtime +$DAYS )
+
+log "backup started"
+log "source $SOURE_DIR"
+log "desti $DEST_DIR"
+log "$DAYS"
+
+
+
+if [ -z "$FILES" ]; then
+
+log "No Files"
+exit 1
+
+else 
+
+log "Files found to archive $FILES"
+
+TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
+ZIPFILE_NAME=$DEST_DIR/app-logs-$TIMESTAMP.tar.gz
+tar -czf $ZIPFILE_NAME $FILES
+
+if [ -f $ZIPFILE_NAME ]; then
+
+log " archive is sucess"
+
+else
+
+log "arcive failure"
+
+fi
+
+
+
+
+
+
+
+
+
+
