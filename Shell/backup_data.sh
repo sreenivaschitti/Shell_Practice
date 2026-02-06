9cfe7d906f92f10f6a2673a496195c58927efe7e
sreenivas <sreenivaschitti@gmail.com>
2026-02-06 16:51:16 +0530

    practice

--- Shell/backup_data.sh
+++ Shell/backup_data.sh
@@ -70,7 +70,8 @@ if [ -z "$FILES" ]; then
     TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
     ZIP_FILE_NAME="$DEST_DIR/app-logs-$TIMESTAMP.tar.gz"
     log "Archieve name: $ZIP_FILE_NAME"
-    find "$SOURE_DIR" -type f -mtime +"$DAYS" -print0 | tar --null -czvf $ZIP_FILE_NAME --files-from=-
+    
+    tar -zcvf $ZIP_FILE_NAME $(find $SOURCE_DIR  -type f -mtime +$DAYS)
 
 fi
 
