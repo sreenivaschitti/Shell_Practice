6e9449db12fc88a908e6a472cf3630c9a7beb07b
sreenivas <sreenivaschitti@gmail.com>
2026-02-06 18:59:38 +0530

    practice

--- Shell/disk.sh
+++ Shell/disk.sh
@@ -1,13 +1,30 @@
-#!bin/bash
 
-THRESHOLD=3
+#!/bin/bash
 
-df -HT | grep -v Filesystem | while IFS= read -r line; do
-    # Split fields
-    USAGE=$(echo "$line" | awk '{print $5}' | tr -d '%')
-    DISK=$(echo "$line" | awk '{print $7}')
-    
-    if [ $USAGE -ge $THRESHOLD ]; then
-        echo "Warning: Disk usage is high on $DISK ($USAGE%)"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+MESSAGE=""
+IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+
+log(){
+    echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $1"
+}
+
+DISK_USAGE=$(df -hT | grep -v Filesystem)
+USAGE_THRESHOLD=3
+
+while IFS= read -r line
+do
+    USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | awk '{print $7}')
+
+    if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then
+        MESSAGE+="High Disk usage on $PARTITION: $USAGE% <br>"
     fi
-done
+done <<< $DISK_USAGE
+
+echo -e "$MESSAGE"
+
+#sh mail.sh "info@joindevops.com" "High Disk Usage Alert on $IP_ADDRESS" "$MESSAGE" "HIGH_DISK_USAGE" "$IP_ADDRESS" "DevOps Team"
