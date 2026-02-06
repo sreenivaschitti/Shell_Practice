a6011bb2f1449339256d6b5181d255075a375b0d
sreenivas <sreenivaschitti@gmail.com>
2026-02-06 18:33:33 +0530

    practice

--- Shell/disk.sh
+++ Shell/disk.sh
@@ -3,3 +3,18 @@
 DISKUSAGE=$( df -HT | grep -v Filesystem | awk '{print $6}' | cut -d '%' -f1)
 
 echo $DISKUSAGE
+
+DISH_TRESHHOLD=3
+
+while IFS= read -r value; do
+
+    USAGE=$( df -HT | grep -v Filesystem | awk '{print $6}' | cut -d '%' -f1)
+    DISK=$( df -HT | grep -v Filesystem | awk '{print $7}' )
+
+    echo "value: $value"
+    if [ $value -ge $DISH_TRESHHOLD ]; then
+    Message+="High disk $USAGE:$DISK" 
+    fi
+done < $DISKUSAGE
+
+echo "$Message"
