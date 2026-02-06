d2027e52cc33a20804f5a238b77429cab4d39b07
sreenivas <sreenivaschitti@gmail.com>
2026-02-06 18:56:58 +0530

    practice

--- Shell/disk.sh
+++ Shell/disk.sh
@@ -7,7 +7,7 @@ df -HT | grep -v Filesystem | while IFS= read -r line; do
     USAGE=$(echo "$line" | awk '{print $5}' | tr -d '%')
     DISK=$(echo "$line" | awk '{print $7}')
     
-    if [ "$USAGE" -ge "$THRESHOLD" ]; then
+    if [ $USAGE -ge $THRESHOLD ]; then
         echo "Warning: Disk usage is high on $DISK ($USAGE%)"
     fi
 done
